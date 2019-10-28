5630371748998e60b196569b752e3bc2ac8f8359
rongtongjin <794220751@qq.com>
2019-10-28 18:52:23 +0800

    Update Test

--- test/rocketmq_send_receive_test.go
+++ test/rocketmq_send_receive_test.go
@@ -54,6 +54,51 @@ func TestRocketMQSendSyncAndReceive(t *testing.T) {
 	}
 }
 
+func TestRocketMQSendAndReceiveChinese(t *testing.T) {
+	flag := false
+	ch := make(chan interface{})
+	producer, err := createRocketMQProducer()
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	err = producer.Start()
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	defer producer.Shutdown()
+	res, err := producer.SendMessageSync(createMessage(NormalTopic, "中文"))
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	if res.Status != rocketmq.SendOK {
+		t.Fatalf("send message fail")
+	}
+	consumer, err := createRocketMQPushConsumer()
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	consumer.Subscribe(NormalTopic, "*", func(msg *rocketmq.MessageExt) rocketmq.ConsumeStatus {
+		t.Log(msg.Body)
+		if msg.Body == "中文" {
+			flag = true
+		}
+		ch <- "done"
+		return rocketmq.ConsumeSuccess
+	})
+	err = consumer.Start()
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	defer consumer.Shutdown()
+	select {
+	case <-time.After(time.Second * 40):
+	case <-ch:
+	}
+	if !flag {
+		t.Errorf("send sync and receive test fail")
+	}
+}
+
 func TestRocketMQSendOnewayAndReceive(t *testing.T) {
 	flag := false
 	ch := make(chan interface{})
